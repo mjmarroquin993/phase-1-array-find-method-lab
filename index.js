383bf65a55e570ad03344936850e16fa4681175b
Josh Marroquin <mj.marroquin993@gmail.com>
2022-04-04 13:14:04 -0700

    commit tree cb32add923b4d5f6ae901eddffc79ee789d28f2f

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+// const found = array1.find(element => element > 10);
+
+
+const superbowlWin = record => {
+const yearWon = record.find(obj => obj.result === "W")
+if (yearWon) {
+    return yearWon.year
+}
+}
+
